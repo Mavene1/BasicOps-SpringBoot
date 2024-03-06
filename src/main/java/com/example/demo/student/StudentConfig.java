4eceff036895f06d85b9db55a99b2cb88bd9f4d1
“Mavene1” <maveneomondi111@gmail.com>
2024-03-06 21:31:01 +0300

    add student config

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    3L,
+                    "Mariam",
+                    23,
+                    LocalDate.of(2000, 01, 10),
+                    "mariam@gmail.com"
+            );
+            Student janes = new Student(
+                    3L,
+                    "Janes",
+                    27,
+                    LocalDate.of(2000, 01, 10),
+                    "janes@gmail.com"
+            );
+            Student mavene = new Student(
+                    3L,
+                    "Mavene",
+                    21,
+                    LocalDate.of(2000, 01, 10),
+                    "mavene@gmail.com"
+            );
+            repository.saveAll(
+                    List.of(mariam, janes, mavene));
+            
+        };
+    }
+}
